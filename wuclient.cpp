3ad4ae7716777e6bcd7df41f4d8dfcc5f9df72b7
Jeff Miller <jeffmill@microsoft.com>
2023-03-02 12:54:50 -0800

    Update wuclient.cpp

--- wuclient.cpp
+++ wuclient.cpp
@@ -16,10 +16,7 @@
         }                                                                                        \
     }
 
-CComModule _Module;
-extern __declspec(selectany) CAtlModule* _pAtlModule = &_Module;
-
-class CSearchCompletedCallback : 
+class CSearchCompletedCallback :
     public CComObjectRootEx<CComMultiThreadModelNoCS>,
     public ISearchCompletedCallback
 {
@@ -30,7 +27,7 @@ public:
 
     DECLARE_NO_REGISTRY()
 
-    CSearchCompletedCallback()
+        CSearchCompletedCallback()
     {
         eventCompleted = CreateEvent(nullptr /*lpEventAttributes*/, TRUE/*bManualReset*/, FALSE/*bInitialState*/, nullptr/*lpName*/);
     }
@@ -40,7 +37,7 @@ public:
         CloseHandle(eventCompleted);
     }
 
-    STDMETHOD(Invoke)(ISearchJob* job,  ISearchCompletedCallbackArgs* callbackArgs)
+    STDMETHOD(Invoke)(ISearchJob* job, ISearchCompletedCallbackArgs* callbackArgs)
     {
         UNREFERENCED_PARAMETER(job);
         UNREFERENCED_PARAMETER(callbackArgs);
@@ -59,74 +56,87 @@ private:
     HANDLE eventCompleted = nullptr;
 };
 
-
-HRESULT Run()
+class CAtlExeModule : public CAtlExeModuleT<CAtlExeModule>
 {
-    std::cout << "Creating IUpdateSession" << std::endl;
-    CComPtr<IUpdateSession> session;
-    CHR(session.CoCreateInstance(__uuidof(UpdateSession), nullptr, CLSCTX_INPROC_SERVER));
-
-    std::cout << "Creating IUpdateSearcher" << std::endl;
-    CComPtr<IUpdateSearcher> searcher;
-    CHR(session->CreateUpdateSearcher(&searcher));
-
-    CComPtr<CSearchCompletedCallback> searchCompletedCallback(new CComObjectNoLock<CSearchCompletedCallback>());
-
-    std::cout << "Calling IUpdateSearcher::BeginSearch" << std::endl;
-    CComBSTR searchCriteria(L"IsInstalled=1");
-    CComVariant state;
-    CComPtr<ISearchJob> searchJob;
-    CHR(searcher->BeginSearch(searchCriteria, searchCompletedCallback, state, &searchJob));
-
-    std::cout << "Waiting for completion" << std::endl;
-    searchCompletedCallback->WaitForCompletion();
-
-    std::cout << "Calling IUpdateSearcher::EndSearch" << std::endl;
-    CComPtr<ISearchResult> searchResult;
-    // This will fail with 80240032 (WU_E_INVALID_CRITERIA) if searchCriteria is invalid.
-    CHR(searcher->EndSearch(searchJob, &searchResult));
-
-    std::cout << "Calling ISearchResult::get_ResultCode" << std::endl;
-    OperationResultCode searchResultCode;
-    CHR(searchResult->get_ResultCode(&searchResultCode));
-    std::cout << "ResultCode = " << searchResultCode << std::endl;
-
-    if (searchResultCode != orcSucceeded)
+public:
+    HRESULT PreMessageLoop(_In_ int /*nShowCmd*/) throw()
     {
-        return E_FAIL;
+        return S_OK;
     }
 
-    CComPtr<IUpdateCollection> updateCollection;
-    CHR(searchResult->get_Updates(&updateCollection));
-    LONG cUpdates;
-    CHR(updateCollection->get_Count(&cUpdates));
-    std::cout << "Update count = " << cUpdates << std::endl;
-
-    for (LONG iUpdate = 0; iUpdate < cUpdates; iUpdate++)
+    void RunMessageLoop() throw()
     {
-        CComPtr<IUpdate> update;
-        CHR(updateCollection->get_Item(iUpdate, &update));
-
-        CComBSTR title;
-        CHR(update->get_Title(&title));
-
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> utf16_to_utf8;
-
-        std::cout << iUpdate + 1 << ": " << utf16_to_utf8.to_bytes(title) << std::endl;
+        Search();
     }
 
-    return S_OK;
-}
+    HRESULT PostMessageLoop() throw()
+    {
+        return S_OK;
+    }
+
+private:
+    HRESULT Search()
+    {
+        std::cout << "Creating IUpdateSession" << std::endl;
+        CComPtr<IUpdateSession> session;
+        CHR(session.CoCreateInstance(__uuidof(UpdateSession), nullptr, CLSCTX_INPROC_SERVER));
+
+        std::cout << "Creating IUpdateSearcher" << std::endl;
+        CComPtr<IUpdateSearcher> searcher;
+        CHR(session->CreateUpdateSearcher(&searcher));
+
+        CComPtr<CSearchCompletedCallback> searchCompletedCallback(new CComObjectNoLock<CSearchCompletedCallback>());
+
+        std::cout << "Calling IUpdateSearcher::BeginSearch" << std::endl;
+        CComBSTR searchCriteria(L"IsInstalled=1");
+        CComVariant state;
+        CComPtr<ISearchJob> searchJob;
+        CHR(searcher->BeginSearch(searchCriteria, searchCompletedCallback, state, &searchJob));
+
+        std::cout << "Waiting for completion" << std::endl;
+        searchCompletedCallback->WaitForCompletion();
+
+        std::cout << "Calling IUpdateSearcher::EndSearch" << std::endl;
+        CComPtr<ISearchResult> searchResult;
+        // This will fail with 80240032 (WU_E_INVALID_CRITERIA) if searchCriteria is invalid.
+        CHR(searcher->EndSearch(searchJob, &searchResult));
+
+        std::cout << "Calling ISearchResult::get_ResultCode" << std::endl;
+        OperationResultCode searchResultCode;
+        CHR(searchResult->get_ResultCode(&searchResultCode));
+        std::cout << "ResultCode = " << searchResultCode << std::endl;
+
+        if (searchResultCode != orcSucceeded)
+        {
+            return E_FAIL;
+        }
+
+        CComPtr<IUpdateCollection> updateCollection;
+        CHR(searchResult->get_Updates(&updateCollection));
+        LONG cUpdates;
+        CHR(updateCollection->get_Count(&cUpdates));
+        std::cout << "Update count = " << cUpdates << std::endl;
+
+        for (LONG iUpdate = 0; iUpdate < cUpdates; iUpdate++)
+        {
+            CComPtr<IUpdate> update;
+            CHR(updateCollection->get_Item(iUpdate, &update));
+
+            CComBSTR title;
+            CHR(update->get_Title(&title));
+
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> utf16_to_utf8;
+
+            std::cout << iUpdate + 1 << ": " << utf16_to_utf8.to_bytes(title) << std::endl;
+        }
+
+        return S_OK;
+	}
+} _Module;
+//extern __declspec(selectany) CAtlModule* _pAtlModule = &_Module;
 
 int main()
 {
-    (void)CoInitializeEx(NULL, COINIT_MULTITHREADED);
-
-    std::cout << "wu client app started" << std::endl;
-
-    Run();
-
-    std::cout << "wu client app ended" << std::endl;
-
-    CoUninitialize();
+    return _Module.WinMain(0);
+    // CoUninitialize();
 }
